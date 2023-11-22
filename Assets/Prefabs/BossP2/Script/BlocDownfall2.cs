28698c604def38892224a5cbc8837962072e9de4
ancario <anthonylabbe68@gmail.com>
2023-11-22 01:03:24 -0500

    début de finitionBoss
    c

---
+++ Assets/Prefabs/BossP2/Script/BlocDownfall2.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocDownfall2 : MonoBehaviour
+{
+    [SerializeField] float HauteurChute = 40f;
+    Vector3 initial;
+    float speed;
+    bool Tombers = false;
+    new AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+        initial = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Tombers)
+        {
+            gameObject.transform.Translate(new Vector3(0, -1, 0) * speed * Time.deltaTime, Space.World);
+            if (transform.position.y < initial.y)
+            {
+                transform.position = initial;
+                Tombers = !Tombers;
+                audio.Play();
+            }
+        }
+    }
+
+    public void Tomber(float speed)
+    {
+        this.speed = speed;
+        Tombers = true;
+    }
+    public void tp()
+    {
+
+        gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + HauteurChute, transform.position.z);
+    }
+}
--- Assets/Prefabs/BossP2/Script/Entrerp2.cs
+++ Assets/Prefabs/BossP2/Script/Entrerp2.cs
@@ -13,20 +13,26 @@ public class Entrerp2 : MonoBehaviour
  
     GameObject Boss;
     List<BlocDownfall> BlocDownfallList = new List<BlocDownfall>();
+    List<BlocDownfall2> BlocDownfallList2 = new List<BlocDownfall2>();
+
     BlocDownfall[] BlocDownfallListsTab = new BlocDownfall[0];
+    BlocDownfall2[] BlocDownfallListsTab2 = new BlocDownfall2[0];
     int ListeRequis;
     // Start is called before the first frame update
     void Start()
     {
         Boss = GameObject.FindGameObjectWithTag("BossP2");
         BlocDownfallListsTab = Boss.GetComponentsInChildren<BlocDownfall>();
+        BlocDownfallListsTab2 = Boss.GetComponentsInChildren<BlocDownfall2>();
         for (int i = 0; i < BlocDownfallListsTab.Length; i++)
         {
-         
-           
             BlocDownfallList.Add(BlocDownfallListsTab[i]);  
         }
-       
+        for (int j = 0; j < BlocDownfallListsTab2.Length; j++)
+        {
+            BlocDownfallList2.Add(BlocDownfallListsTab2[j]);
+        }
+
 
     }
 
@@ -45,14 +51,20 @@ public class Entrerp2 : MonoBehaviour
     [SerializeField] int blocParBPM=5;
     [SerializeField] int augmentationDuTauxNBDrop =1;
     int compteurAugmentation;
+      int pointeur;
+    bool reset =true;
     private void Update()
     {
 
-
+        Debug.Log(BlocDownfallList2.Count);
         if (Tp)
         {
 
             foreach (BlocDownfall t in BlocDownfallList)
+            {
+                t.tp();
+            }
+            foreach (BlocDownfall2 t in BlocDownfallList2)
             {
                 t.tp();
             }
@@ -66,13 +78,13 @@ public class Entrerp2 : MonoBehaviour
             {
                 for (int i = 0; i < blocParBPM && BlocDownfallList.Count != 0; i++)
                 {
-                    int pointeur;
+
                     pointeur = random.Next(0, BlocDownfallList.Count);
-                 
+
                     BlocDownfallList[pointeur].Tomber(vitesseBPM);
                     BlocDownfallList.Remove(BlocDownfallList[pointeur]);
                     ConteurBPM = 0;
-                    
+
 
                 }
                 vitesseBPM += 2;
@@ -81,7 +93,7 @@ public class Entrerp2 : MonoBehaviour
                 {
                     BPM -= 0.02f;
                 }
-                else 
+                else
                 {
                     if (compteurAugmentation > augmentationDuTauxNBDrop)
                     {
@@ -92,7 +104,7 @@ public class Entrerp2 : MonoBehaviour
                     {
                         compteurAugmentation++;
                     }
-                       
+
                 }
             }
             else
@@ -101,12 +113,55 @@ public class Entrerp2 : MonoBehaviour
                 ConteurBPM += Time.deltaTime;
             }
         }
+        else if (BlocDownfallList2.Count != 0)
+        {
+            if (reset)
+            {
+                reset = !reset;
+                ConteurBPM = 0;
+                BPM = 0.4f;
+                blocParBPM= 5;
+                vitesseBPM = 50;
+            }
+            if (ConteurBPM >= BPM)
+            {
+                for (int j = 0; j < blocParBPM && BlocDownfallList2.Count != 0; j++)
+                {
+
+                    pointeur = random.Next(0, BlocDownfallList2.Count);
+
+                    BlocDownfallList2[pointeur].Tomber(vitesseBPM);
+                    BlocDownfallList2.Remove(BlocDownfallList2[pointeur]);
+                    ConteurBPM = 0;
 
 
+                }
+                vitesseBPM += 2;
+
+                if (BPM - 0.02f > 0.2f)
+                {
+                    BPM -= 0.02f;
+                }
+                else
+                {
+                    if (compteurAugmentation > augmentationDuTauxNBDrop)
+                    {
+                        blocParBPM += 1;
+                        compteurAugmentation = 0;
+                    }
+                    else
+                    {
+                        compteurAugmentation++;
+                    }
+
+                }
+            }
+
+
+
+
+        }
 
 
     }
-
-
-
 }

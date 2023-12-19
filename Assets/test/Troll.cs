21dfbd1e31a4c689408c165be8017a489a24d46c
ancario <anthonylabbe68@gmail.com>
2023-12-19 08:41:19 -0500

    c
    c

---
+++ Assets/test/Troll.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Troll : MonoBehaviour
+{
+    public void troll (){
+
+        if (true == true)
+        {
+            if (true == true)
+            {
+                if (true == true)
+                {
+                    if (true == true)
+                    {
+                        if (true == true)
+                        {
+                            if (true == true)
+                            {
+                                if (true == true)
+                                {
+                                    if (true == true)
+                                    {
+                                        if (true == true)
+                                        {
+                                            if (true == true)
+                                            {
+                                                if (true == true)
+                                                {
+                                                    if (true == true)
+                                                    {
+                                                        if (true == true)
+                                                        {
+                                                            if (true == true)
+                                                            {
+                                                                if (true == true)
+                                                                {
+                                                                    if (true == true)
+                                                                    {
+
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        }
+}
